0b05611f3cde18fcce9519fcc0c7f454d8e73cfe
tkbacher <tiffanybacher@gmail.com>
2019-04-13 12:30:22 -0600

    Set up imports for testing and scss

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
+import Nav from '../components/Nav';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-        Memoize
-          
+          <Nav />
         </header>
       </div>
     );
---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Nav extends Component {
+  render() {
+    return (
+      <nav>
+        <h2>Memoize</h2>
+      </nav>
+    );
+  }
+}
+
+export default Nav;
