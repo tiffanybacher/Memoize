2cb54b2ffdda2650de68315df9b5b46eb12acbbc
tkbacher <tiffanybacher@gmail.com>
2019-04-15 13:03:10 -0600

    Fetch data

--- src/components/App.js
+++ src/components/App.js
@@ -5,50 +5,66 @@ import Flashcard from '../components/Flashcard';
 import AnswerInput from '../components/AnswerInput';
 import UserAnswer from '../components/UserAnswer';
 
-let data = require('../data.js');
+// let data = require('../data.js');
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       group: 1,
-      question: data.objectQuestions[0].question,
+      cardQuestions: '',
+      question: '',
       answer: '',
       displayAnswer: false,
       userAnswer: ''
     }
   }
 
+  componentDidMount() {
+    fetch('https://fe-apps.herokuapp.com/api/v1/memoize/1901/tiffanybacher/objectquestions')
+      .then(response => response.json())
+      .then(data => this.setState({
+        cardQuestions: data.objectQuestions,
+        question: data.objectQuestions[0].question}))
+      .catch(err => console.log(err));
+  }
+
   findAnswer = () => {
     this.setState({
-      answer: data.objectQuestions[0].answer,
+      answer: this.state.cardQuestions[0].answer,
       displayAnswer: true
-    }, () => {console.log(this.state.answer)});
+    });
   }
 
   getUserAnswer = (answerInput) => {
     this.setState({
       userAnswer: answerInput
-    }, () => console.log(this.state.userAnswer));
+    });
   }
 
   render() {
+    console.log(this.state.cardQuestions)
+
+    let answerInput;
+    let instructions;
+    let header;
+
     if (this.state.displayAnswer) {
-      var answerInput = 
+      answerInput = 
         <UserAnswer userAnswer={this.state.userAnswer}/>
-      var instructions =
+      instructions =
         <p>If you feel good about your answer, click the top button to move on! If you think you may need more review, click the bottom button to come back to this question later.</p>
-      var header = 
+      header = 
         <h2>Check your answer!</h2>
     } else {
-      var answerInput = 
+      answerInput = 
         <AnswerInput 
           findAnswer={this.findAnswer}
           getUserAnswer={this.getUserAnswer}
         />
-      var instructions = 
+      instructions = 
         <p>Answer the question on the flashcard below. Submit your answer first then check your answer!</p>
-      var header =
+      header =
         <h2>Learn About Objects and Classes!</h2>
     }
 
