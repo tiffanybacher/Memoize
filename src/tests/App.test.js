b30c6221193cf82cf21463a3a848e4145f8ff734
tkbacher <tiffanybacher@gmail.com>
2019-04-14 19:18:25 -0600

    Update Nav test to add state

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from '../components/App';
 
 describe('App', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(
+      <App />
+    );
+  });
+
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 
-  it.skip('should match snapshot', () => {
-    
+  it('should match snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
   });
 
   it.skip('should have a default state', () => {
--- src/tests/Nav.test.js
+++ src/tests/Nav.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import Nav from '../components/Nav';
 
 describe('Nav', () => {
+  let wrapper; 
+  
+  beforeEach(() => {
+    wrapper = shallow(
+      <Nav />
+    );
+  });
+
   it('should match snapshot', () => {
-    
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should have default state', () => {
+    expect(wrapper.state()).toEqual({
+      slide: 0
+    });
   });
 });
